216b611a5f5eadb0087a89c19774adebf189292b
Mariusz <mariusz.tripler@gmail.com>
2016-11-12 08:32:28 +0000

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -91,7 +91,6 @@
    
    var messageTextbox = jQuery('[name=message]');
    socket.emit('createMessage',{
-    from: 'User',
     text: messageTextbox.val()
    }, function() {
      messageTextbox.val('');
